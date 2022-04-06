e101ceff165693c034f7c78a59422dbf43bd701e
AjayMorde <103033106+AjayMorde@users.noreply.github.com>
2022-04-06 15:39:35 +0530

    Add files via upload
    intial commit

---
+++ IfElse.java
@@ -0,0 +1,42 @@
+package welcomeToJava;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int  y = 6;
+		int z= 7;
+		if(x>y) {
+		if(x>z)  {
+			System.out.println("x is greater");
+		}
+		else
+		{
+			System.out.println("z is greater");
+		}
+		}
+		else if(y>x)
+		{
+			
+		if(y>z)
+		{
+		System.out.println("y is greater");
+		}
+		else
+		{
+			System.out.println("z is greater");
+			
+		}
+		}
+		
+		
+		
+		
+		
+				
+
+	}
+
+}
+
